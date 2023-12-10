4ada35eda91b9cb93b8033cf89136b246cdbfc74
Neithan <nathan.carlos@lavid.ufpb.br>
2023-12-10 08:40:28 -0300

    Update minute condition in main.py

--- main.py
+++ main.py
@@ -367,7 +367,7 @@ while True:
             condicao_Automl = 0
             # condicao_Randomf = 0
 
-            if minute > 1 and minute < 45:
+            if minute > 10 and minute < 45:
                 try:
 
                     # Xht_transform = preprocessor_league.transform(Xht_league)
